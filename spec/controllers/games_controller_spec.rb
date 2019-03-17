09ad5f86f1bf3622fa16bdab976bbb1f09ccd99f
Combos93 <combos93@mail.ru>
2019-03-17 19:17:44 +0600

    add controller tests

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -61,5 +61,43 @@ RSpec.describe GamesController, type: :controller do
       expect(response).to redirect_to(game_path(game))
       expect(flash.empty?).to be_truthy
     end
+
+    it 'does not #show game' do
+      alien_game = FactoryBot.create(:game_with_questions)
+
+      get :show, params: { id: alien_game.id }
+
+      expect(response.status).not_to eq 200
+      expect(response).to redirect_to(root_path)
+      expect(flash[:alert]).to be
+    end
+
+    it 'take_money before end game' do
+      game_w_questions.update_attribute(:current_level, 2)
+
+      put :take_money, params: { id: game_w_questions.id }
+      game = assigns(:game)
+
+      expect(game.finished?).to be_truthy
+      expect(game.prize).to eq(200)
+
+      user.reload
+
+      expect(user.balance).to eq(200)
+
+      expect(response).to redirect_to(user_path(user))
+      expect(flash[:warning]).to be
+    end
+
+    it 'do not play two games together' do
+      expect(game_w_questions.finished?).to be_falsey
+      expect { post :create }.to change(Game, :count).by(0)
+
+      game = assigns(:game)
+      expect(game).to be_nil
+
+      expect(response).to redirect_to(game_path(game_w_questions))
+      expect(flash[:alert]).to be
+    end
   end
 end
