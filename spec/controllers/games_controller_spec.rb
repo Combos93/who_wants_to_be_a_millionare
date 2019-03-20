1cb6b6be84d6e43164e2d5141d0ea7fa1debbdf4
Combos93 <combos93@mail.ru>
2019-03-20 23:17:45 +0600

    can you use help 50/50

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -151,5 +151,22 @@ RSpec.describe GamesController, type: :controller do
 
       expect(response).to redirect_to(game_path(game))
     end
+
+    it "uses fifty_fifty" do
+      expect(game_w_questions.current_game_question.help_hash[:fifty_fifty]).not_to be
+      expect(game_w_questions.friend_call_used).to be_falsey
+
+      put :help, params: { id: game_w_questions.id, help_type: :fifty_fifty }
+      game = assigns(:game)
+
+      expect(game.finished?).to be_falsey
+      expect(game.fifty_fifty_used).to be_truthy
+      expect(game.current_game_question.help_hash[:fifty_fifty]).to be
+
+      help_ff = game.current_game_question.help_hash[:fifty_fifty]
+      expect(help_ff).to include('b')
+
+      expect(response).to redirect_to(game_path(game))
+    end
   end
 end
