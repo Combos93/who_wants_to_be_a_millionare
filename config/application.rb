03b7c9516dd232f1da523c683cbf7182309b5692
Combos93 <combos93@mail.ru>
2019-03-20 23:01:14 +0600

    release helps & tdd_rspec_tests

--- config/application.rb
+++ config/application.rb
@@ -8,25 +8,15 @@ Bundler.require(*Rails.groups)
 
 module Billionaire
   class Application < Rails::Application
+    Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = true
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = true
-
-    config.i18n.default_locale = :ru
-
-    config.assets.initialize_on_precompile = true
-
-    # Don't generate system test files.
-    config.generators.system_tests = nil
-
-    config.time_zone = 'Almaty'
-    # Но это Омск
 
     config.i18n.default_locale = :ru
     config.i18n.locale = :ru
-
-    I18n.config.available_locales = :ru
-
     config.i18n.fallbacks = [:en]
+
+    config.time_zone = 'Moscow'
+
   end
 end
--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -22,11 +22,65 @@ RSpec.describe GameQuestion, type: :model do
       expect(game_question.level).to eq(game_question.question.level)
       expect(game_question.text).to eq(game_question.question.text)
     end
+
+    it 'correct .help_hash' do
+      # на фабрике у нас изначально хэш пустой
+      expect(game_question.help_hash).to eq({})
+
+      # добавляем пару ключей
+      game_question.help_hash[:some_key1] = 'blabla1'
+      game_question.help_hash['some_key2'] = 'blabla2'
+
+      # сохраняем модель и ожидаем сохранения хорошего
+      expect(game_question.save).to be_truthy
+
+      # загрузим этот же вопрос из базы для чистоты эксперимента
+      gq = GameQuestion.find(game_question.id)
+
+      # проверяем новые значение хэша
+      expect(gq.help_hash).to eq({some_key1: 'blabla1', 'some_key2' => 'blabla2'})
+    end
   end
 
   context "method correct_answer_key" do
     it 'return key of right answer' do
      expect(game_question.correct_answer_key).to eq 'b'
+    end
+  end
+
+  context "user helpers" do
+    it 'correct audience_help' do
+      expect(game_question.help_hash).not_to include(:audience_help)
+
+      game_question.add_audience_help
+
+      expect(game_question.help_hash).to include(:audience_help)
+      expect(game_question.help_hash[:audience_help].keys).to contain_exactly('a', 'b', 'c', 'd')
+    end
+
+    it "correct fifty_fifty" do
+      expect(game_question.help_hash).not_to include(:fifty_fifty)
+
+      game_question.add_fifty_fifty
+
+      expect(game_question.help_hash).to include(:fifty_fifty)
+
+      help_ff = game_question.help_hash[:fifty_fifty]
+
+      expect(help_ff).to include('b')
+      expect(help_ff.size).to be 2
+    end
+
+    it "correct friend_call" do
+      expect(game_question.help_hash).not_to include(:friend_call)
+
+      game_question.add_friend_call
+
+      expect(game_question.help_hash).to include(:friend_call)
+
+      help_fc = game_question.help_hash[:friend_call]
+
+      expect(help_fc).to include("считает, что это вариант")
     end
   end
 end
