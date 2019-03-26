eb413d34a5d8eee21665e02aba694e75de505b8a
Combos93 <combos93@mail.ru>
2019-03-26 12:49:56 +0600

    fix partials test

--- spec/views/user/show.html.erb_spec.rb
+++ spec/views/user/show.html.erb_spec.rb
@@ -21,6 +21,26 @@ RSpec.describe 'users/show', type: :view do
     it 'renders button for change password' do
       expect(rendered).to match 'Сменить имя и пароль'
     end
+  end
+
+  context "when other user look alien profile" do
+    before(:each) do
+      @user = FactoryBot.create(:user, name: "Vasiliy")
+      @games = [FactoryBot.build_stubbed(:game, id: 1, created_at: Time.now, current_level: 6)]
+
+      assign(:user, @user)
+      assign(:games, @games)
+
+      render
+    end
+
+    it 'renders user name' do
+      expect(rendered).to match "Vasiliy"
+    end
+
+    it 'does not render button for change password' do
+      expect(rendered).not_to match "Сменить имя и пароль"
+    end
 
     it 'renders partial _game' do
       assert_template partial: "users/_game", count: @games.count
